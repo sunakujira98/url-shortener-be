74c552f7968782b8b8445e5b0a6bbc6dc99c9617
Steven Theodore <steven@Stevens-MacBook-Pro.local>
2025-01-29 13:36:01 +0700

    add more cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://cendekin.netlify.app'],
   }),
 )
 app.use(bodyParser.urlencoded({ extended: true }))
